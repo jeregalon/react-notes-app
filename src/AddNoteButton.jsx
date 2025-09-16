4a5de9704612c4f4ec4799e3b4ce85625f6cd320
Jose_Enrique <jeregalon@gmail.com>
2025-09-16 13:29:05 -0400

    components created

---
+++ src/AddNoteButton.jsx
@@ -0,0 +1,7 @@
+export default function AddNoteButton() {
+  return (
+    <button className="bg-neutral-800 rounded-lg flex items-center justify-center text-6xl font-bold p-8 text-white hover:bg-neutral-700 transition">
+      +
+    </button>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import NotePreview from "./NotePreview";
+import AddNoteButton from "./AddNoteButton";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="min-h-screen bg-black text-white p-8">
+      <h1 className="text-3xl mb-6">Notas</h1>
 
-export default App
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <AddNoteButton />
+
+        <NotePreview
+          title="Mi primera nota"
+          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+          date="16 de septiembre"
+        />
+
+       
+      </div>
+    </div>
+  );
+}
