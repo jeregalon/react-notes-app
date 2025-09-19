4ee3cd1026c1909099b42e78eba1f36bdf479da9
Jose_Enrique <jeregalon@gmail.com>
2025-09-19 12:52:52 -0400

    fixed addNoteButton height

--- src/AddNoteButton.jsx
+++ src/AddNoteButton.jsx
@@ -1,6 +1,6 @@
 export default function AddNoteButton({ onClick }) {
   const buttonAnimation = "transition transform active:scale-95 duration-150 ease-in-out"
-  const className = `bg-neutral-800 rounded-lg flex items-center justify-center text-6xl font-bold p-8 text-white hover:bg-neutral-700 transition cursor-pointer ${buttonAnimation}`
+  const className = `bg-neutral-800 rounded-lg min-h-[220px] flex items-center justify-center text-6xl font-bold p-8 text-white hover:bg-neutral-700 transition cursor-pointer ${buttonAnimation}`
   return (
     <button 
       onClick={onClick}
